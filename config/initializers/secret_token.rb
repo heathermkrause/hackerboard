a2b8362ab23a432f2ed9b3fec3aad849dda6ee23
Heather Krause <hthr.krause@gmail.com>
2013-03-04 20:27:45 -0500

    Initial Push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackboard::Application.config.secret_token = 'b21bbb89d8a0a1725c4998c74a08b2464392c5472f8cf56180cf7998744711d64a84bcce1720f9d559ded1dc437b21301743a8abd1b4d6bdad2957b6196dd264'
