53b88cd75ece313228dac18b9d20ed3ac81048f1
Heather Krause <hthr.krause@gmail.com>
2013-03-04 20:43:01 -0500

    Next update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,20 @@ class PostsController < ApplicationController
   def index
       @posts = Post.all
   end
+
+  def new
+    @posts = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    if @post.valid?
+      @post.save
+      redirect_to posts_path
+    else
+      render :new  #render instead or redirect so that the data and errors are still there
+end
+
+end
+
 end
